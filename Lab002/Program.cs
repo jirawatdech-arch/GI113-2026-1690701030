484a62b5befc3190ec7ee439080a8012dc146eab
jirawatdech-arch <jirawat.dech@bumail.net>
2026-08-30 04:23:20 +0700

    Update Program.cs

--- Lab002/Program.cs
+++ Lab002/Program.cs
@@ -1,4 +1,14 @@
-﻿namespace Lab002
+﻿/*
+ * Student ID :1690701030
+ * Name       :jirawat dechprom
+ * Section    :129A
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+
+
+namespace Lab002
 {
     internal class Program
     {
